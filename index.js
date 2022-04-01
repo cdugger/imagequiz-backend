15597c13b5902ccf65cc2574bc571abae153b576
cdugger <cduggerk@gmail.com>
2022-03-31 17:55:51 -0700

    Finished api calls

--- index.js
+++ index.js
@@ -7,37 +7,73 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).json({done: true, message: 'Fine!'});
+    res.status(200).json({ done: true, message: 'Hello world from the backend API' });
 })
 
 app.post('/register', (req, res) => {
     let name = req.body.name;
     let email = req.body.email;
     let password = req.body.password;
-    store.addCustomer(name, email, password);
-    res.status(200).json({done: true, message: 'Customer added'});
+    let result = store.addCustomer(name, email, password);
+    if (result.valid) {
+        res.status(200).json({ done: true, message: 'Customer added' });
+    } else {
+        res.status(403).json({ done: false, message: result.message });
+    }
 })
 
 app.post('/login', (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
     let result = store.login(email, password);
-    if(result.valid) {
-        res.status(200).json({done: true, message: 'The customer logged in successfully'});
+    if (result.valid) {
+        res.status(200).json({ done: true, message: 'The customer logged in successfully' });
     } else {
-        res.status(401).json({done: false, message: result.message})
+        res.status(401).json({ done: false, message: result.message });
+    }
+})
+
+app.get('/flowers', (req, res) => {
+    let result = store.getFlowers();
+    if (result.done) {
+        res.status(200).json({ done: true, message: 'Successfully retrieved flowers', result: result.flowers })
+    } else {
+        res.status(404).json({ done: false, message: result.message })
     }
 })
 
 app.get('/quiz/:id', (req, res) => {
     let id = req.params.id;
     let result = store.getQuiz(id);
-    if(result.done) {
-        res.status(200).json({done: true, result: result.quiz});
+    if (result.done) {
+        res.status(200).json({ done: true, result: result.quiz });
     } else {
-        res.status(404).json({done: false, message: result.message});
+        res.status(404).json({ done: false, message: result.message, result: undefined });
     }
 });
+
+app.post('/score', (req, res) => {
+    let quizTaker = req.body.quizTaker;
+    let quizId = req.body.quizId;
+    let quizScore = req.body.quizScore;
+    let result = store.addScore(quizTaker, quizId, quizScore);
+    if (result.valid) {
+        res.status(200).json({ done: true, message: 'Quiz score successfully added.' });
+    } else {
+        res.status(401).json({ done: false, message: result.message });
+    }
+})
+
+app.get('/scores/:quiztaker/:quizid', (req, res) => {
+    let quizTaker = req.params.quiztaker;
+    let quizId = req.params.quizid;
+    let result = store.getScore(quizTaker, quizId);
+    if (result.valid) {
+        res.status(200).json({ done: true, result: result.score });
+    } else {
+        res.status(404).json({ done: false, message: result.message});
+    }
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- temp-store/store.js
+++ temp-store/store.js
@@ -1,36 +1,76 @@
 const bcrypt = require('bcrypt');
 let { customers } = require('./customers');
-let {quizzes} = require('./data');
+let { quizzes } = require('./data');
+let { flowers } = require('./flowers');
+let { scores } = require('./scores');
+
 let store = {
     addCustomer: (name, email, password) => {
-        const hash = bcrypt.hashSync(password, 10);
-        customers.push({id: 1, name: name, email: email, password: hash})
+        // check if this email is already in use
+        let customer = customers.find(x => x.email.toLowerCase() === email.toLowerCase());
+        if (!customer) {
+            const hash = bcrypt.hashSync(password, 10);
+            customers.push({ id: 1, name: name, email: email, password: hash })
+            return { valid: true, message: 'Successfully registered.' }
+        } else {
+            return { valid: false, message: 'Email already in use.' }
+        }
     },
 
     login: (email, password) => {
         let customer = customers.find(x => x.email.toLowerCase() === email.toLowerCase());
-        if(customer) {
+        if (customer) {
             let valid = bcrypt.compareSync(password, customer.password);
-            if(valid) {
-                return {valid: true};
+            if (valid) {
+                return { valid: true };
             } else {
-                return {valid: false, message: 'Credentials are not valid.'}
+                return { valid: false, message: 'Credentials are not valid.' }
             }
         } else {
-            return {valid: false, message: 'Email not found.'}
+            return { valid: false, message: 'Email not found.' }
+        }
+    },
+
+    getFlowers: () => {
+        let flower_list = flowers;
+        if (flower_list) {
+            return { done: true, flowers };
+        } else {
+            return { done: false, message: 'Error retrieving flowers.' };
         }
     },
 
     getQuiz: (id) => {
         let quiz = quizzes.find(x => x.name.toLowerCase() === id.toLowerCase());
-        if(quiz) {
-            return {done: true, quiz};
+        if (quiz) {
+            return { done: true, quiz };
         } else {
-            return {done: false, message: 'A quiz with the name was not found.'};
+            return { done: false, message: 'A quiz with the name was not found.' };
+        }
+    },
+
+    addScore: (quizTaker, quizId, quizScore) => {
+        let isValidCustomer = customers.find(x => x.email.toLowerCase() === quizTaker.toLowerCase());
+        let isValidQuiz = quizzes.find(x => x.name === quizId);
+        if(isValidCustomer && isValidQuiz) {
+            let currentDate = new Date();
+            scores.push({quizTaker: quizTaker, quizId: quizId, score: quizScore, date: currentDate.toJSON()});
+            return {valid: true}
+        } else if(!isValidCustomer) {
+            return {valid: false, message: 'The given email does not match a customer in our records.'}
+        } else {
+            return {valid: false, message: 'The given quiz ID does not match a quiz in our records.'}
+        }
+    },
+
+    getScore: (quizTaker, quizId) => {
+        let score = scores.find(x => {x.quizTaker.toLowerCase() === quizTaker && x.quizId === quizId});
+        if(score) {
+            return {valid: true, result: score }
+        } else {
+            return {valid: false, message: 'Score not found.'}
         }
     }
-
-
 }
 
 module.exports = { store };
